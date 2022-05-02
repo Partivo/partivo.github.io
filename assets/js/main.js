faa7f30b513fd0612fcce21fa5810aa9b869b548
Fantros <krostotr@gmail.com>
2022-05-02 15:50:35 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ function writeData(data) {
 	setTimeout(function() {
 		document.getElementById("root").style.display = "block"
 		document.getElementById("loading").style.display = "none";
-	}, 3000);
+	}, 5000);
 }
 
 function randomString(len, charSet) {
